c39e28847b65e205d19fc5ab5e52ac3e364e3559
sumaiaakter <sumaiamannan2013@gmail.com>
2021-09-14 09:43:48 -0500

    color-added

--- js/app.js
+++ js/app.js
@@ -14,17 +14,18 @@ const showProducts = (products) => {
   for (const product of allProducts) {
     const image = product.images;
     const div = document.createElement("div");
+    // div.style.backgroundColor = 'burlywood';
     div.classList.add("product");
     div.innerHTML = `
     <div>
       <img class="product-image" src=${product.image}> 
-      <h3>${product.title}</h3>
+      <h4>${product.title}</h4>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
       <h4>Rate: ${product.rating.rate}</h4>
       <h4>Count: ${product.rating.count}</h4>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-info">add to cart</button>
+      <button id="details-btn" class="btn btn-primary">Details</button>
     </div>
       `;
     document.getElementById("all-products").appendChild(div);
